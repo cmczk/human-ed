ac97bb5e878ce9533bfe10cf6d93f812b286e364
Yauheni Kuntsevich <cmaczok@gmail.com>
2025-09-14 18:42:38 +0300

    init sqlite storage

--- human-ed-api/cmd/human-ed-api/main.go
+++ human-ed-api/cmd/human-ed-api/main.go
@@ -4,13 +4,16 @@ import (
 	"log/slog"
 
 	"github.com/cmczk/human-ed/internal/config"
+	"github.com/cmczk/human-ed/internal/storage/sqlite"
 	"github.com/cmczk/human-ed/lib/logger"
 )
 
 func main() {
 	cfg := config.MustLoad()
-
 	log := logger.Setup(cfg.Env)
+
+	storage := sqlite.New(cfg.StoragePath)
+	_ = storage
 
 	log.Info("application is starting", slog.String("env", cfg.Env))
 	log.Debug("debug messages are enabled")
--- human-ed-api/internal/storage/sqlite/sqlite.go
+++ human-ed-api/internal/storage/sqlite/sqlite.go
@@ -1,11 +1,27 @@
 package sqlite
 
-import "database/sql"
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
 
 type Storage struct {
 	DB *sql.DB
 }
 
-func New() *Storage {
-	return &Storage{}
+func New(storagePath string) *Storage {
+	const op = "storage.sqlite.New"
+
+	db, err := sql.Open("sqlite3", storagePath)
+	if err != nil {
+		log.Fatalf("%s: cannot open connection for sqlite db: %s", op, err.Error())
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("%s: ping failed: %s", op, err.Error())
+	}
+
+	return &Storage{DB: db}
 }
